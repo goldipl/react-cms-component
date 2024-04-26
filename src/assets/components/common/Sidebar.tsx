0fa30ba202c8b56e4ad0db1ac4b30f7b26614b7e
goldipl <mgodlewski92@gmail.com>
2024-04-26 10:39:47 +0200

    add sidebar tags modal

--- src/assets/components/common/Sidebar.tsx
+++ src/assets/components/common/Sidebar.tsx
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import sidebarInfoIcon from "./../../icons/sidebar/sidebar-info.svg";
 import sidebarChatIcon from "./../../icons/sidebar/sidebar-chat.svg";
 import sidebarSettingsIcon from "./../../icons/sidebar/sidebar-settings.svg";
 import sidebarLabelIcon from "./../../icons/sidebar/sidebar-label.svg";
 import sidebarUserSharedIcon from "./../../icons/sidebar/sidebar-user-shared.svg";
+import SidebarTagsModal from "./SidebarTagsModal";
 
 const Sidebar = () => {
+  const [showTagsModal, setShowTagsModal] = useState(false);
+
+  const handleTagsModalClick = () => {
+    setShowTagsModal(!showTagsModal);
+  };
+
   return (
-    <div className="flex flex-col border border-1 border-[#E1E1E2] bg-white rounded-xl p-2 w-max h-auto">
-      <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
-        <img src={sidebarInfoIcon} alt="sidebar icon" width={32} height={32} />
+    <div className="relative">
+      {/* Sidebar container */}
+      <div className="flex flex-col border border-1 border-[#E1E1E2] bg-white rounded-xl p-2 w-max h-auto">
+        <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
+          <img
+            src={sidebarInfoIcon}
+            alt="sidebar icon"
+            width={32}
+            height={32}
+          />
+        </div>
+        <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
+          <img
+            src={sidebarChatIcon}
+            alt="sidebar icon"
+            width={32}
+            height={32}
+          />
+        </div>
+        <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
+          <img
+            src={sidebarSettingsIcon}
+            alt="sidebar icon"
+            width={32}
+            height={32}
+          />
+        </div>
+        <div
+          className={`flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]`}
+          onClick={handleTagsModalClick}
+        >
+          <img
+            src={sidebarLabelIcon}
+            alt="sidebar icon"
+            width={32}
+            height={32}
+          />
+        </div>
+        <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
+          <img
+            src={sidebarUserSharedIcon}
+            alt="sidebar icon"
+            width={32}
+            height={32}
+          />
+        </div>
       </div>
-      <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
-        <img src={sidebarChatIcon} alt="sidebar icon" width={32} height={32} />
-      </div>
-      <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
-        <img
-          src={sidebarSettingsIcon}
-          alt="sidebar icon"
-          width={32}
-          height={32}
-        />
-      </div>
-      <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
-        <img src={sidebarLabelIcon} alt="sidebar icon" width={32} height={32} />
-      </div>
-      <div className="flex p-4 rounded-xl cursor-pointer hover:bg-[#F4F4F5]">
-        <img
-          src={sidebarUserSharedIcon}
-          alt="sidebar icon"
-          width={32}
-          height={32}
-        />
+      {/* Sidebar Modals container */}
+      <div className="flex absolute top-0 left-24">
+        {showTagsModal && <SidebarTagsModal />}
       </div>
     </div>
   );
---
+++ src/assets/components/common/SidebarTagsModal.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import closeIcon from "./../../icons/sidebar_modal/close-x.svg";
+
+const SidebarTagsModal = () => {
+  const [modalVisible, setModalVisible] = useState(true);
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <div
+      className={`flex flex-col border border-1 border-[#E1E1E2] bg-white rounded-xl w-max h-auto min-w-[387px] p-4 ${
+        modalVisible ? "flex" : "hidden"
+      }`}
+    >
+      <div className="flex flex-row justify-between items-center">
+        <h2 className="font-bold text-2xl">Tagi</h2>
+        <span className="cursor-pointer" onClick={handleCloseModal}>
+          <img src={closeIcon} alt="close-icon" width={32} height={32} />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarTagsModal;
