34f8243cb59d1dac78d09b45567f7ac9d7bac703
goldipl <mgodlewski92@gmail.com>
2024-04-27 14:59:35 +0200

    styling searchbar and scroll results

--- src/components/sidebar_tags/Searchbar.tsx
+++ src/components/sidebar_tags/Searchbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import data from "./../../data/tags.json";
+import searchIcon from "./../../assets/icons/sidebar_modal/search.svg";
 
 interface Tag {
   id: number;
@@ -29,21 +30,31 @@ const TagsComponent: React.FC = () => {
 
   return (
     <div>
-      <h2>Tags</h2>
-      <input
-        type="text"
-        placeholder="Search tags..."
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <ul>
+      <div className="flex flex-row items-center justify-start pt-0 p-2">
+        <img
+          className="me-2"
+          src={searchIcon}
+          alt="search icon"
+          width={24}
+          height={24}
+        />
+        <input
+          className="focus:outline-none focus:border-gray-300 focus:ring-1 focus:ring-gray-300 rounded-md p-2 w-full font-medium"
+          type="text"
+          placeholder="Wyszukaj grupę lub tag"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <ul className=" overflow-y-auto max-h-80">
         {filteredTags.map((tag) => (
-          <li key={tag.id}>
-            <label>
+          <li key={tag.id} className="flex items-center mb-1 ">
+            <label className="flex w-full items-center text-sm rounded-md font-medium text-gray-900 dark:text-gray-300 select-none h-9 px-2 py-4 hover:bg-[#F4F4F5]">
               <input
                 type="checkbox"
                 checked={tag.checked}
                 onChange={() => handleTagToggle(tag.id)}
+                className="w-5 h-5 me-2 text-blue-600 bg-gray-100 border-gray-300 rounded-2xl focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               />
               {tag.name}
             </label>
