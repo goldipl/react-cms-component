5c761c2f79901de80d8b7e405bba5491ba061756
goldipl <mgodlewski92@gmail.com>
2024-05-10 11:54:06 +0200

    refactor code, seperate qualityLevels to a new component

---
+++ src/components/sidebar_tags/TagsQuality/QualityLevels.tsx
@@ -0,0 +1,42 @@
+export const QualityLevels = [
+  {
+    text: "Źle",
+    statusColor: "text-red-700",
+    progressBarColor: "bg-red-700",
+    statusText:
+      "Zbyt mało tagów. Dodaj jeszcze 3 aby poprawić widoczność artykułu",
+  },
+  {
+    text: "Słabo",
+    statusColor: "text-red-400",
+    progressBarColor: "bg-red-400",
+    statusText:
+      "Zbyt mało tagów. Dodaj jeszcze 2 aby poprawić widoczność artykułu",
+  },
+  {
+    text: "Ok",
+    statusColor: "text-yellow-400",
+    progressBarColor: "bg-yellow-400",
+    statusText:
+      "Jest ok. Jak dodasz więcej tagów to widoczność artykułu się poprawi",
+  },
+  {
+    text: "Dobrze",
+    statusColor: "text-green-600",
+    progressBarColor: "bg-green-600",
+    statusText:
+      "Jest dobrze. Jak dodasz 1 tag więcej to widoczność artykułu znacznie się poprawi",
+  },
+  {
+    text: "Bardzo Dobrze",
+    statusColor: "text-lime-500",
+    progressBarColor: "bg-lime-500",
+    statusText: "Jest bardzo dobrze. O to chodziło",
+  },
+  {
+    text: "Wybierz Tagi",
+    statusColor: "text-gray-600",
+    progressBarColor: "bg-gray-600",
+    statusText: "",
+  },
+];
--- src/components/sidebar_tags/TagsQuality/TagsQuality.tsx
+++ src/components/sidebar_tags/TagsQuality/TagsQuality.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import infoIcon from "./../../../assets/icons/sidebar_modal/info.svg";
+import { QualityLevels } from "./QualityLevels";
 
 interface TagsQualityProps {
   checkedCount: number;
@@ -13,41 +14,13 @@ const TagsQuality: React.FC<TagsQualityProps> = ({ checkedCount }) => {
   let qualityProgressBarColor = "";
   let qualityStatusText = "";
 
-  if (checkedCount === 1) {
-    qualityText = "Źle";
-    qualityStatusColor = "text-red-700";
-    qualityProgressBarColor = "bg-red-700";
-    qualityStatusText =
-      "Zbyt mało tagów. Dodaj jeszcze 3 aby poprawić widoczność artykułu";
-  } else if (checkedCount === 2) {
-    qualityText = "Słabo";
-    qualityStatusColor = "text-red-400";
-    qualityProgressBarColor = "bg-red-400";
-    qualityStatusText =
-      "Zbyt mało tagów. Dodaj jeszcze 2 aby poprawić widoczność artykułu";
-  } else if (checkedCount === 3) {
-    qualityText = "Ok";
-    qualityStatusColor = "text-yellow-400";
-    qualityProgressBarColor = "bg-yellow-400";
-    qualityStatusText =
-      "Jest ok. Jak dodasz więcej tagów to widoczność artykułu się poprawi";
-  } else if (checkedCount === 4) {
-    qualityText = "Dobrze";
-    qualityStatusColor = "text-green-600";
-    qualityProgressBarColor = "bg-green-600";
-    qualityStatusText =
-      "Jest dobrze. Jak dodasz 1 tag więcej to widoczność artykułu znacznie się poprawi";
-  } else if (checkedCount >= 5) {
-    qualityText = "Bardzo Dobrze";
-    qualityStatusColor = "text-lime-500";
-    qualityProgressBarColor = "bg-lime-500";
-    qualityStatusText = "Jest bardzo dobrze. O to chodziło";
-  } else {
-    qualityText = "Wybierz Tagi";
-    qualityStatusColor = "text-gray-600";
-    qualityProgressBarColor = "bg-gray-600";
-    qualityStatusText = "";
-  }
+  const checkedCountIndex = Math.min(Math.max(checkedCount, 0), 5);
+  const qualityLevel = QualityLevels[checkedCountIndex];
+
+  qualityText = qualityLevel.text;
+  qualityStatusColor = qualityLevel.statusColor;
+  qualityProgressBarColor = qualityLevel.progressBarColor;
+  qualityStatusText = qualityLevel.statusText;
 
   const handleButtonClick = () => {
     setShowQuality(!showQuality);
